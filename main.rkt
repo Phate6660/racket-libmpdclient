a150a3f70288396314fc7b198320ce7ac1b0e745
ValleyKnight <ValleyKnight@protonmail.com>
2021-07-16 13:46:22 -0400

    add mpd_send_pause

--- main.rkt
+++ main.rkt
@@ -8,10 +8,13 @@
 (define-ffi-definer  define-libmpdclient (ffi-lib "libmpdclient"))
 (define-cstruct      _mpd_connection     ([*conn _string]))
 (define-libmpdclient mpd_connection_new  (_fun _string _int _int -> _mpd_connection))
+(define-libmpdclient mpd_send_pause      (_fun _mpd_connection _bool -> _bool))
 (define-libmpdclient mpd_send_stop       (_fun _mpd_connection -> _bool))
 
 ;; To connect to MPD with the default settings:
 ;; (define conn (mpd_connection_new "localhost" 6600 1000))
 
 ;; Examples of basic commands:
-;; (mpd_send_stop conn)
+;; (mpd_send_stop conn) -- stops music
+;; (mpd_send_pause conn #t) -- pauses music
+;; (mpd_send_pause conn #f) -- resumes music
