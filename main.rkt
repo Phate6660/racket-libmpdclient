287145181dac24e223b33e0752ad332606108755
ValleyKnight <ValleyKnight@protonmail.com>
2021-07-16 17:02:09 -0400

    provide the functions to be used as a lib

--- main.rkt
+++ main.rkt
@@ -13,11 +13,22 @@
 (define-libmpdclient mpd_run_next         (_fun _mpd_connection              -> _bool))
 (define-libmpdclient mpd_run_pause        (_fun _mpd_connection _bool        -> _bool))
 (define-libmpdclient mpd_run_previous     (_fun _mpd_connection              -> _bool))
-(define-libmpdclient mpd_run_repeat       (_fun _mpd_connection _bool        -> _bool))
 (define-libmpdclient mpd_run_random       (_fun _mpd_connection _bool        -> _bool))
+(define-libmpdclient mpd_run_repeat       (_fun _mpd_connection _bool        -> _bool))
 (define-libmpdclient mpd_run_seek_current (_fun _mpd_connection _float _bool -> _bool))
 (define-libmpdclient mpd_run_single       (_fun _mpd_connection _bool        -> _bool))
 (define-libmpdclient mpd_run_stop         (_fun _mpd_connection              -> _bool))
+(provide mpd_connection_new
+         mpd_run_consume
+         mpd_run_crossfade
+         mpd_run_next
+         mpd_run_pause
+         mpd_run_previous
+         mpd_run_random
+         mpd_run_repeat
+         mpd_run_seek_current
+         mpd_run_single
+         mpd_run_stop)
 
 ;; To connect to MPD with the default settings:
 ;; (define conn (mpd_connection_new "localhost" 6600 1000))
